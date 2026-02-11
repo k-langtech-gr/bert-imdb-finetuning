e102c88a0bfedd51beb1a57db9df3892a3fb6a71
k_gas_nlp <gasteratoukonstantina@gmail.com>
2026-02-11 19:49:18 +0200

    Add compute_metrics function for accuracy evaluation

---
+++ src/metrics.py
@@ -0,0 +1,9 @@
+import numpy as np
+import evaluate
+
+accuracy = evaluate.load("accuracy")
+
+def compute_metrics(eval_pred):
+    logits, labels = eval_pred
+    preds = np.argmax(logits, axis=-1)
+    return accuracy.compute(predictions=preds, references=labels)
